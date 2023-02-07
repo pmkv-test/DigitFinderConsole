b33568d07b62b194205334ae43480f9bbbe96793
apimkov <pag2002@gmail.com>
2023-02-07 15:49:02 +0300

    Format code

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,21 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//извлечь числа из текста
 public class Main {
-    public static final String REGEXP_CONST="\\d{1,10}";
+    public static final String REGEXP_CONST = "\\d{1,10}";
 
-    public static void main(String[] args) throws IOException
-    {
-        int intSum=0;
-        System.out.print("Введите текст:");
-        BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in));
-        String[] arraySplitText = buffReader.readLine().split(" ");
-        for(String splitText: arraySplitText){
+    public static void main(String[] args) throws IOException {
+        int intSum = 0;
+        for (String splitText : args) {
             Pattern pattern = Pattern.compile(REGEXP_CONST);
             Matcher matcher = pattern.matcher(splitText);
             while (matcher.find()) {
                 String sMatchGroup = (matcher.group(0));
-                intSum=intSum+Integer.parseInt(sMatchGroup);
+                intSum = intSum + Integer.parseInt(sMatchGroup);
                 System.out.println(matcher.group(0));
             }
         }
-        System.out.println("Сумма="+intSum);
+        System.out.println("Total=" + intSum);
     }
 }
